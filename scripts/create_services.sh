0f61ae16a8f28f040f21e0b4503e91347052b5a8
Olli Janatuinen <olli.janatuinen@gmail.com>
2018-05-01 20:55:59 +0300

    Generating even more events with test services

--- scripts/create_services.sh
+++ scripts/create_services.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-swarmctl -s /tmp/swarmkit-stress-tester/manager1.sock service create --name foo --mode global --constraint node.role==worker --image nginx
+swarmctl -s /tmp/swarmkit-stress-tester/manager1.sock service create --name foo --mode global --constraint node.role==worker --image alpine --command ping --args 9.9.9.9
 swarmctl -s /tmp/swarmkit-stress-tester/manager1.sock service create --name bar --replicas 2 --constraint node.role==worker --image alpine --command ping --args 8.8.8.8
