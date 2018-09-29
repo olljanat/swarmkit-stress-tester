c467da461c6e7b8fe91d9b1dcc1d87235cd02f58
Olli Janatuinen <olli.janatuinen@gmail.com>
2018-09-29 15:49:39 +0000

    Aded --replicas-max-per-node switch to test services

--- scripts/create_services.sh
+++ scripts/create_services.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-swarmctl -s /tmp/swarmkit-stress-tester/manager1.sock service create --name foo --mode global --constraint node.role==worker --image test-service
-swarmctl -s /tmp/swarmkit-stress-tester/manager1.sock service create --name bar --replicas 2 --constraint node.role==worker --image test-service
+swarmctl -s /tmp/swarmkit-stress-tester/manager1.sock service create --name foo --replicas 2 --replicas-max-per-node 1 --constraint node.role==worker --image test-service
+swarmctl -s /tmp/swarmkit-stress-tester/manager1.sock service create --name bar --replicas 2 --replicas-max-per-node 1 --constraint node.role==worker --image test-service
 
